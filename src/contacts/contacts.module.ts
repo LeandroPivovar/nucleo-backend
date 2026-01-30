29d59981e0d368d47a382ae808ab0cc76c933214
LeandroPivovar <leandropivovar@hotmail.com>
2026-01-30 10:20:12 -0300

    feat

--- src/contacts/contacts.module.ts
+++ src/contacts/contacts.module.ts
@@ -7,12 +7,13 @@ import { ContactTag } from '../entities/contact-tag.entity';
 import { ContactSegmentation } from '../entities/contact-segmentation.entity';
 import { Tag } from '../entities/tag.entity';
 import { Group } from '../entities/group.entity';
+import { ContactPurchase } from '../entities/contact-purchase.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Contact, ContactTag, ContactSegmentation, Tag, Group])],
+  imports: [TypeOrmModule.forFeature([Contact, ContactTag, ContactSegmentation, Tag, Group, ContactPurchase])],
   controllers: [ContactsController],
   providers: [ContactsService],
   exports: [ContactsService],
 })
-export class ContactsModule {}
+export class ContactsModule { }
 

dada08a8757f2076699c6405761acc89f4c25547
LeandroPivovar <leandropivovar@hotmail.com>
2026-03-04 18:39:16 -0300

    chore: force sender emailContato@nucleocrm.com.br

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -33,8 +33,8 @@ export class EmailService {
     const apiToken = this.configService.get<string>('ZENVIA_API_TOKEN', '');
     const smsFrom = this.configService.get<string>('ZENVIA_SMS_FROM', '');
 
-    const fromEmail = dbFromEmail || this.configService.get<string>('SMTP_FROM_EMAIL') || this.configService.get<string>('SMTP_FROM') || smsFrom || 'contato@nucleocrm.com.br';
-    const fromName = dbFromName || this.configService.get<string>('SMTP_FROM_NAME') || 'Núcleo CRM';
+    const fromEmail = 'contato@nucleocrm.com.br';
+    const fromName = 'Núcleo CRM';
 
     return { apiToken, fromEmail, fromName };
   }

6bc34d51f278314ac7c964c9390f0073ebee077e
LeandroPivovar <leandropivovar@hotmail.com>
2026-03-12 19:48:30 -0300

    feat

--- src/nuvemshop/nuvemshop.module.ts
+++ src/nuvemshop/nuvemshop.module.ts
@@ -3,13 +3,16 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { NuvemshopController } from './nuvemshop.controller';
 import { NuvemshopService } from './nuvemshop.service';
 import { NuvemshopConnection } from '../entities/nuvemshop-connection.entity';
+import { Contact } from '../entities/contact.entity';
+import { Sale } from '../entities/sale.entity';
+import { Product } from '../entities/product.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([NuvemshopConnection])],
+  imports: [TypeOrmModule.forFeature([NuvemshopConnection, Contact, Sale, Product])],
   controllers: [NuvemshopController],
   providers: [NuvemshopService],
   exports: [NuvemshopService],
 })
-export class NuvemshopModule {}
+export class NuvemshopModule { }
 
 
